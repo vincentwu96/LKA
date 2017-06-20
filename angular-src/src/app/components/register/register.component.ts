82fccba3950eec90c2228a270a849adb132d82c1
vincentwu96 <vincentwu96@yahoo.com>
2017-06-20 12:26:55 -0700

    first commit

---
+++ angular-src/src/app/components/register/register.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import {AuthService} from '../../services/auth.service'
+import {Router} from '@angular/router';
+
+@Component({
+selector: 'app-register',
+templateUrl: './register.component.html',
+styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+name: String;
+username: String;
+email: String;
+password: String;
+
+
+constructor(
+private validateService: ValidateService,
+private authService:AuthService,
+private router: Router
+) { }
+
+ngOnInit() {
+}
+
+onRegisterSubmit(){
+const user = {
+name: this.name,
+email: this.email,
+username: this.username,
+password: this.password
+}
+
+// Required Fields
+if(!this.validateService.validateRegister(user)){
+alert('Please fill in all fields');
+return false;
+}
+
+
+// Validate Email
+if(!this.validateService.validateEmail(user.email)){
+alert('Please use a valid email');
+return false;
+}
+
+// Register user
+this.authService.registerUser(user).subscribe(data => {
+	if(data.success){
+	alert('You are now registered');
+	this.router.navigate(['/login']);
+	}else{
+	alert('Something went wrong');
+	this.router.navigate(['/register']);
+	}
+});
+
+}
+}
