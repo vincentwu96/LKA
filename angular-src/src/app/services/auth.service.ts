bdf5e7824a7496891f87245d65eafad1f43d9866
vincentwu96 <vincentwu96@yahoo.com>
2017-06-22 17:13:24 -0700

    updated home page

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -14,14 +14,14 @@ constructor(private http:Http) { }
 registerUser(user){
 let headers = new Headers();
 headers.append('Content-Type','application/json');
-return this.http.post('http://localhost:3000/users/register', user,{headers: headers})
+return this.http.post('users/register', user,{headers: headers})
 .map(res => res.json());
 }
 
 authenticateUser(user){
 let headers = new Headers();
 headers.append('Content-Type','application/json');
-return this.http.post('http://localhost:3000/users/authenticate', user,{headers: headers})
+return this.http.post('users/authenticate', user,{headers: headers})
 .map(res => res.json());
 }
 
@@ -30,7 +30,7 @@ let headers = new Headers();
 this.loadToken();
 headers.append('Authorization', this.authToken);
 headers.append('Content-Type','application/json');
-return this.http.get('http://localhost:3000/users/profile',{headers: headers})
+return this.http.get('users/profile',{headers: headers})
 .map(res => res.json());
 }
 
