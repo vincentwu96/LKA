fa5171df6b279cc8334ac54f9a62ec2407f4965d
vincentwu96 <vincentwu96@yahoo.com>
2017-09-05 16:45:25 -0700

    fixed navbar for mobile

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,23 @@ import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 
 
+
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+selector: 'app-navbar',
+templateUrl: './navbar.component.html',
+styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 fullImagePath: string;
+
+
+
+isIn = false;   // store state
+toggleState() { // click handler
+let bool = this.isIn;
+this.isIn = bool === false ? true : false; 
+}
+
 
 constructor(
 private authService:AuthService,
@@ -18,13 +28,13 @@ private router:Router
 this.fullImagePath = '/assets/images/LKA.png'
 }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+}
 
-  onLogoutClick(){
-	this.authService.logout();
-	// User logged out
-	this.router.navigate(['/login']);
-	return false;
-  }
+onLogoutClick(){
+this.authService.logout();
+// User logged out
+this.router.navigate(['/login']);
+return false;
+}
 }
